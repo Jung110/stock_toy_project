95d280b0a5d46dc0285d94a39f0b28b10c399787
Jung110 <aoemsltm119@gmail.com>
2023-10-10 23:28:29 +0900

    feat: bug fix

--- Mysql/daily.py
+++ Mysql/daily.py
@@ -2,16 +2,26 @@ import requests
 import pandas as pd
 import time
 from sqlalchemy import create_engine
+from datetime import datetime
+
 
 if __name__ == "__main__":
     # connect to  MySql DataBase
-    db_connection_str = 'mysql+pymysql://root:@localhost/stockDB'
+    # db_connection_str = 'mysql+pymysql://root:@localhost/stockDB'
+    # for home
+    db_connection_str = 'mysql+pymysql://root:1234@localhost/stock_db'
     db_connection = create_engine(db_connection_str)
     conn = db_connection.connect()
 
+    ## 날짜 가져오기
+    beginBasDt=f'{datetime.today().year}{datetime.today().month }{datetime.today().day}'
+
     # request data to API
     headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
-    key_path = "/Users/c05/Desktop/learn/web/PipeLine_Project/key.txt"
+    # for school
+    # key_path = "/Users/c05/Desktop/learn/web/PipeLine_Project/key.txt"
+    # for home
+    key_path = "C:\\Users\\AW17R4\\.appkey\\open_stock_api_key.txt"
     url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
 
 
@@ -26,25 +36,26 @@ if __name__ == "__main__":
             }
 
     response = requests.get(url ,params=params)
-    total_count = response.json()['response']['body']['totalCount'] 
+    total_count = response.json()['response']['body']['totalCount']
 
 
     df = pd.DataFrame(response.json()['response']['body']['items']['item'])
-    df.to_sql(name='stockInfoTable', con=db_connection, if_exists='replace',index=False)  
+    df.to_sql(name='stockinfotable', con=db_connection, if_exists='append',index=False)  
     if total_count > params['numOfRows']:
         for i in range(2,total_count//10000+1):
             params['pageNo'] = i
 
-            try: 
-                response = requests.get(url ,params=params)
-            except Exception:
-                time.sleep(1)
-                response = requests.get(url ,params=params)
-            # print(i)
-            try:
-                temp_df =  pd.DataFrame(response.json()['response']['body']['items']['item'])
-                temp_df.to_sql(name='stockInfoTable', con=db_connection, if_exists='append',index=False)
-            except Exception:
-                print(temp_df.head(1))
-                print('error')
-                continue
+            while True:
+                errorCount = 1
+                try:
+                    response = requests.get(url ,params=params)
+                    temp_df =  pd.DataFrame(response.json()['response']['body']['items']['item'])
+                    temp_df.to_sql(name='stockinfotable', con=db_connection, if_exists='append',index=False)
+                    break
+                except Exception:
+                    time.sleep(5)
+                    print(f'error:{errorCount}')
+                    if errorCount==10:
+                        break
+                    continue
+            time.sleep(0.5)
